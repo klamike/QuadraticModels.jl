1a338e460b81106e9d91554eb23916b7c622a637
Klamkin, Michael <mklamkin3@gatech.edu>
2026-03-26 12:38:16 -0400

    skip beta if zero?

--- src/batch_spmv.jl
+++ src/batch_spmv.jl
@@ -80,13 +80,14 @@ function _batch_spmv_impl!(
 ) where T
   nout = length(op.rowptr) - 1
   bs = size(out, 2)
+  beta_is_zero = iszero(beta)
   @inbounds for r in 1:nout
     for j in 1:bs
       acc = zero(T)
       for k in op.rowptr[r]:op.rowptr[r+1]-1
         acc += op.nzVals[op.flat_nz[k], j] * B[op.flat_val[k] + val_offset, j]
       end
-      out[r, j] = alpha * acc + beta * out[r, j]
+      out[r, j] = beta_is_zero ? alpha * acc : alpha * acc + beta * out[r, j]
     end
   end
   return out
